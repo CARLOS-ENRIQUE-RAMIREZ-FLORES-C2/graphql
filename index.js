778acdb5e0f4511bf8cd80eaf7606de3c1e665ea
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-02 22:13:31 -0500

    se agregi express js  y express-graphql

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-'use strict'
+"use strict";
 
+const { graphql, buildSchema } = require("graphql");
+const express = require("express");
+const gqlMiddleware = require("express-graphql");
 
-const { graphql, buildSchema } = require('graphql')
+const app = express();
+const port = process.env.port || 3005;
 
 ///definiendo el esquema
 const schema = buildSchema(`
 type Query {
-    hello: String
+    hello: String,
+    saludo: String
 }
-`)
+`);
 
-//Ejecutar el query
+//configurar resolver, se ejecutan justo despues de que se terme de hacer el query
+const resolvers = {
+  hello: () => {
+    return "Hola Mundito!";
+  },
+  saludos: () => {
+    return "Hol a todos";
+  },
+};
 
-graphql(schema, `{hello}`)
-    .then( data => {
-        console.log(data)
-    })
+app.use('/api', gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+  }))
+  
+  app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}/api`);
+  })
